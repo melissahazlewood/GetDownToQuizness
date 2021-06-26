7c9a46f5c319d6e3ec79ba9972847c4f00a88636
tj5775 <tj.5775@gmail.com>
2021-06-25 18:26:51 -0700

    SplashActivity done

--- app/src/main/java/com/example/getdowntoquizness/MainActivity.java
+++ app/src/main/java/com/example/getdowntoquizness/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.getdowntoquizness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,8 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hi");
-        System.out.println("Hi back");
+        //Intent intent = new Intent(this, SplashActivity.class);
+        //startActivity(intent);
+        //System.out.println("Hi");
+        //System.out.println("Hi back");
     }
 
 }
---
+++ app/src/main/java/com/example/getdowntoquizness/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.getdowntoquizness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_layout);
+        int SPLASH_TIME_OUT = 10000;
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Your Code
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+//        new Handler().postDelayed(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+//                startActivity(i);
+//                finish();
+//            }
+//        }, SPLASH_TIME_OUT);
+    }
+}
