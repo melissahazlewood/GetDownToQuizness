968e8fdfd6f0bb14f110d940bc6ea37240aa78db
tj5775 <tj.5775@gmail.com>
2021-06-30 19:59:54 -0700

    Student's log out, about, help, and home DONE

--- app/src/main/java/com/example/getdowntoquizness/OptionsMenuActivityStudent.java
+++ app/src/main/java/com/example/getdowntoquizness/OptionsMenuActivityStudent.java
@@ -1,22 +1,48 @@
 package com.example.getdowntoquizness;
 
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TableLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
 
 public class OptionsMenuActivityStudent extends AppCompatActivity {
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    private AppBarLayout mAppBarLayout;
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragmentTemp = fm.findFragmentById(R.id.fragContStudent);
+        mTabLayout = findViewById(R.id.tabs);
+        mViewPager = findViewById(R.id.view_pager);
+        mAppBarLayout = findViewById(R.id.studentTitle);
         switch (item.getItemId()){
             case R.id.menu_student_home:
+
                 showMessage("Home");
+                if(fragmentTemp != null){
+                    showStudentTabs();
+                    fm.beginTransaction().remove(fragmentTemp).commit();
+                }
                 break;
             case R.id.menu_student_logout:
-                showMessage("Logout");
+                showMessage("You logged out successfully");
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
                 break;
             case R.id.menu_student_editProfile:
                 showMessage("Edit Profile");
@@ -25,15 +51,34 @@ public class OptionsMenuActivityStudent extends AppCompatActivity {
                 showMessage("Change password");
                 break;
             case R.id.menu_student_help:
+                hideStudentTabs();
                 showMessage("Help");
+                fragmentTemp = new HelpFragment();
+                fm.beginTransaction().replace(R.id.fragContStudent, fragmentTemp)
+                        .addToBackStack(null).commit();
                 break;
             case R.id.menu_student_about:
+                hideStudentTabs();
                 showMessage("About");
+                fragmentTemp = new AboutFragment();
+                fm.beginTransaction().replace(R.id.fragContStudent, fragmentTemp)
+                        .addToBackStack(null).commit();
                 break;
             default:
                 return super.onOptionsItemSelected(item);
         }
         return true;
+    }
+    public void hideStudentTabs(){
+        mTabLayout.setVisibility(View.INVISIBLE);
+        mViewPager.setVisibility(View.INVISIBLE);
+        mAppBarLayout.setVisibility(View.INVISIBLE);
+    }
+
+    public void showStudentTabs(){
+        mTabLayout.setVisibility(View.VISIBLE);
+        mViewPager.setVisibility(View.VISIBLE);
+        mAppBarLayout.setVisibility(View.VISIBLE);
     }
 
     @Override
